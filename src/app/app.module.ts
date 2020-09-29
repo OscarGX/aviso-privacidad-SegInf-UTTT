565d9d2ce4d38b7896a8aa92ac82b968b6984ddb
Oscar Reyes <reyesmartinezoscar3g15@gmail.com>
2020-09-28 22:15:40 -0500

    Agregando alertas y aviso de privacidad

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +7,35 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  isAccepted = false;
 
   constructor() { }
 
   ngOnInit() {
+    this.presentModal();
   }
 
+  private async presentModal() {
+    const avisoText = `
+      La seguridad es muy importante para nosotros, es por eso que quermos proteger tus datos.
+      <br>De acuerdo con lo establecido en la<strong> Ley Federal de Protección de Datos Personales en
+      Posesión de los Particulares y su Reglamento</strong>, autorizo a "nombre de la empresa etc" el
+      tratamiento de mis datos personales de acuerdo a lo previsto en el <a href="#">aviso de privacidad.</a>
+    `;
+    const { value: accept } = await Swal.fire({
+      title: 'Aviso de privacidad',
+      html: avisoText,
+      input: 'checkbox',
+      inputPlaceholder: 'He leido el aviso de privacidad y acepto los términos y condiciones',
+      confirmButtonText: 'Aceptar',
+      inputValidator: (result) => {
+        return !result && 'Debes aceptar los términos y condiciones';
+      },
+      allowEscapeKey: false,
+      allowOutsideClick: false
+    });
+    if (accept) {
+      this.isAccepted = true;
+    }
+  }
 }
